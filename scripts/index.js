89854adecc5ec614eafa23f191ff42d2432f92cf
Serge Kislak <kislak7@gmail.com>
2021-04-20 20:04:04 +0300

    fix review issues

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,30 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container');
 
-let nameInput = formElement.querySelector('.popup__field_name');
-let titleInput =  formElement.querySelector('.popup__field_title');
+let nameInput = formElement.querySelector('.popup__field_name_name');
+let titleInput =  formElement.querySelector('.popup__field_name_title');
 let name = document.querySelector('.profile__name-text');
 let title = document.querySelector('.profile__title');
 
 let closeButton = document.querySelector('.popup__close');
 
-function openForm(evt) {
-    evt.preventDefault();
+const openForm = () => {
     nameInput.value = name.textContent;
     titleInput.value = title.textContent;
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
 }
-editButton.addEventListener('click', openForm)
 
-function submitForm (evt) {
+const closeForm = () => {
+    popup.classList.remove('popup_opened');
+}
+
+const submitForm = (evt) => {
     evt.preventDefault();
     name.textContent = nameInput.value;
     title.textContent = titleInput.value;
     closeForm();
 }
-formElement.addEventListener('submit', submitForm);
 
-function closeForm() {
-    popup.classList.remove('popup_opened')
-}
+formElement.addEventListener('submit', submitForm);
+editButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
