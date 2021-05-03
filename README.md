365729910e24384d8ddf48c90190a85cd5a2771c
Serge Kislak <kislak7@gmail.com>
2021-05-03 19:12:25 +0300

    6 iz korobki

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 **Figma**
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+* [Ссылка 2 на макет в Figma](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1)
 
 **Картинки**
 
--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,44 @@ const submitForm = (evt) => {
 formElement.addEventListener('submit', submitForm);
 editButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+const elementTemplate = document.querySelector('#element-template').content;
+const elementsList = document.querySelector('.elements__list');
+
+initialCards.forEach((item) => {
+    const userElement = elementTemplate.querySelector('.element').cloneNode(true);
+    userElement.querySelector('.element__picture').src = item.link;
+    userElement.querySelector('.element__picture').alt = item.name;
+    userElement.querySelector('.element__title').textContent = item.name;
+    elementsList.append(userElement);
+})
+
+
