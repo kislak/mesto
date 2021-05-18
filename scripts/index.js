49e5e02a7192e92743b76615e2dd1bbec39d2be4
serge <kislak7@gmail.com>
2021-05-18 23:20:59 +0300

    cleanup: объявление переменных -> объявление функций -> вызов функций -> установка слушателей

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,20 @@ const profileName = document.querySelector('.profile__name-text');
 const profileTitle = document.querySelector('.profile__title');
 const closeProfilePopupButton = profilePopup.querySelector('.popup__close');
 
+const elementTemplate = document.querySelector('#element-template').content;
+const elementsList = document.querySelector('.elements__list');
+const picturePopup = document.querySelector('.popup_type_image')
+const picturePopupImage = picturePopup.querySelector('.popup__image')
+const picturePopupImageName = picturePopup.querySelector('.popup__image-name')
+const closePicturePopupButton = picturePopup.querySelector('.popup__close');
+
+const addPlaceButton = document.querySelector('.profile__add-button');
+const placePopup = document.querySelector('.popup_type_add-place');
+const placeForm = placePopup.querySelector('.popup__container');
+const placeNameInput = placeForm.querySelector('.popup__field_name_name');
+const placeLinkInput =  placeForm.querySelector('.popup__field_name_link');
+const closePlacePopupButton = placePopup.querySelector('.popup__close');
+
 const openPopup = (popup) => popup.classList.add('popup_opened') ;
 const closePopup = (popup) => popup.classList.remove('popup_opened');
 
@@ -23,67 +37,13 @@ const submitProfile = (evt) => {
     closePopup(profilePopup);
 }
 
-editProfileButton.addEventListener('click', openProfilePopup);
-closeProfilePopupButton.addEventListener('click', () =>  { closePopup(profilePopup) });
-profileForm.addEventListener('submit', submitProfile);
-
-const elementTemplate = document.querySelector('#element-template').content;
-const elementsList = document.querySelector('.elements__list');
-const picturePopup = document.querySelector('.popup_type_image')
-const picturePopupImage = picturePopup.querySelector('.popup__image')
-const picturePopupImageName = picturePopup.querySelector('.popup__image-name')
-const closePicturePopupButton = picturePopup.querySelector('.popup__close');
-
-closePicturePopupButton.addEventListener('click', () => closePopup(picturePopup));
-
-const createCard = (name, link) => {
-    const card = elementTemplate.querySelector('.element').cloneNode(true);
-    const picture = card.querySelector('.element__picture');
-    const title = card.querySelector('.element__title')
-    const heart = card.querySelector('.element__heart')
-    const deleteButton = card.querySelector('.element__delete-button')
-
-    title.textContent = name;
-    picture.src = link;
-    picture.alt = name;
-
-    picture.addEventListener('click', function (evt) {
-        picturePopupImage.src = link
-        picturePopupImage.alt = name;
-        picturePopupImageName.textContent = name;
-        picturePopup.classList.add('popup_opened');
-    });
-
-    heart.addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__heart_active')
-    });
-
-    deleteButton.addEventListener('click', function (evt) {
-        evt.target.closest('.element').remove();
-    });
-
-    return card
-}
-
-const addCard = (card) => elementsList.prepend(card);
-
-initialCards.reverse().forEach((item) => {
-    const card = createCard(item.name, item.link);
-    addCard(card)
-})
-
-const addPlaceButton = document.querySelector('.profile__add-button');
-const placePopup = document.querySelector('.popup_type_add-place');
-const placeForm = placePopup.querySelector('.popup__container');
-const placeNameInput = placeForm.querySelector('.popup__field_name_name');
-const placeLinkInput =  placeForm.querySelector('.popup__field_name_link');
-
 const openPlaceForm = () => {
     placeNameInput.value = "";
     placeLinkInput.value = "";
     openPopup(placePopup)
 }
-addPlaceButton.addEventListener('click', openPlaceForm);
+
+const addCard = (card) => elementsList.prepend(card);
 
 const submitPlaceForm = (evt) => {
     evt.preventDefault();
@@ -91,7 +51,43 @@ const submitPlaceForm = (evt) => {
     addCard(card);
     closePopup(placePopup)
 }
-placeForm.addEventListener('submit', submitPlaceForm);
 
-const closePlacePopupButton = placePopup.querySelector('.popup__close');
+const createCard = (name, link) => {
+    const card = elementTemplate.querySelector('.element').cloneNode(true);
+    const picture = card.querySelector('.element__picture');
+    const title = card.querySelector('.element__title')
+    const heart = card.querySelector('.element__heart')
+    const deleteButton = card.querySelector('.element__delete-button')
+    const pictureClickHandler = () => {
+        picturePopupImage.src = link;
+        picturePopupImage.alt = name;
+        picturePopupImageName.textContent = name;
+        picturePopup.classList.add('popup_opened');
+    };
+
+    const heartClickHandler = (evt) => evt.target.classList.toggle('element__heart_active');
+    const deleteButtonClickHandler = (evt) => evt.target.closest('.element').remove();
+
+    title.textContent = name;
+    picture.src = link;
+    picture.alt = name;
+
+    picture.addEventListener('click', pictureClickHandler);
+    heart.addEventListener('click', heartClickHandler);
+    deleteButton.addEventListener('click', deleteButtonClickHandler);
+
+    return card;
+}
+
+initialCards.reverse().forEach((item) => {
+    const card = createCard(item.name, item.link);
+    addCard(card)
+})
+
+editProfileButton.addEventListener('click', openProfilePopup);
+closeProfilePopupButton.addEventListener('click', () =>  { closePopup(profilePopup) });
+profileForm.addEventListener('submit', submitProfile);
+closePicturePopupButton.addEventListener('click', () => closePopup(picturePopup));
+addPlaceButton.addEventListener('click', openPlaceForm);
+placeForm.addEventListener('submit', submitPlaceForm);
 closePlacePopupButton.addEventListener('click', () => closePopup(placePopup));
