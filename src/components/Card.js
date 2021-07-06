98f2a54cd1350296a421f2c8408083dfaf94ffaa
Serge Kislak <kislak7@gmail.com>
2021-07-06 20:12:52 +0300

    Like count

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ export default class Card {
     constructor(data, templateSelector, handleCardClick) {
         this._name = data.name;
         this._link = data.link;
+        this._likes_length = data.likes.length;
         this._templateSelector = templateSelector;
         this._handleCardClick = handleCardClick;
     }
@@ -11,11 +12,13 @@ export default class Card {
         this._picture = this._card.querySelector('.element__picture');
         this._title = this._card.querySelector('.element__title');
         this._heart = this._card.querySelector('.element__heart');
+        this._likeCounter = this._card.querySelector('.element__like-counter');
         this._deleteButton = this._card.querySelector('.element__delete-button');
 
         this._picture.src = this._link;
         this._picture.alt = this._name;
         this._title.textContent = this._name;
+        this._likeCounter.textContent = this._likes_length;
 
         this._setEventListeners();
 
